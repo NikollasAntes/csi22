973466fd87df0fa56795c8b0066987b91d80dbb4
nikollas <nikollasantes@gmail.com>
2023-03-27 19:55:47 -0300

    questao2

---
+++ lista2/questao2.py
@@ -0,0 +1,13 @@
+# vale ressaltar que no exemplo apresentado no enunciado da questao
+# não se está calculando as médias das tuplas, mas sim a metade da soma
+# a questão foi resolvida considerando-se a média das tuplas
+
+from itertools import zip_longest
+
+def media_tuplas(tuplas, valor_padrao=0):
+    resultado = []
+    for grupo in zip_longest(*tuplas, fillvalue=valor_padrao):
+        soma = sum(valor for valor in grupo if valor != valor_padrao)
+        media = soma / len(grupo)
+        resultado.append(media)
+    return tuple(resultado)
