50a376266ecd029d05bf341982abcdabca739079
nikollas <nikollasantes@gmail.com>
2023-03-29 16:40:34 -0300

    lista2

--- lista2/questao6.py
+++ lista2/questao6.py
@@ -4,9 +4,7 @@ def reverter_lista(lista):
         yield lista[i]
         i -= 1
 
-#lista = [4,7,8]
-#gen = reverter_lista(lista)
-#j = 0
-#while j < len(lista):
-#    print(next(gen))
-#    j += 1
+lista = [4,7,8]
+gen = reverter_lista(lista)
+for i in range(len(lista)):
+    print(next(gen))
